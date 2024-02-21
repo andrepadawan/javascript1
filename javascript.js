087c8f4b863916285354475329adee57a5e9e5f7
andrepadawan <andrea.olmo98@gmail.com>
2024-02-21 07:23:34 +0100

    Fine tutorial

---
+++ javascript.js
@@ -0,0 +1,17 @@
+let message;
+message = "Hello";
+message = "world";
+
+const myBirthday = "24.02.1998";
+const COLOR_RED = "#F00";
+
+
+alert(message);
+
+let admin, name; // can declare two variables at once
+
+na_me = "John";
+
+admin = na_me;
+
+alert( admin ); 
